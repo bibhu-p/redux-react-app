4b672f7a6641dbc3c280c447d548bfe749c62424
Bibhu Prasad <bibhup.behera@nettantra.net>
2022-04-15 17:02:12 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import './App.css'
 import { useSelector, useDispatch } from 'react-redux';
-import { incrementNum, decrementNum } from "./actions/action";
+import { incrementNum, decrementNum } from "./redux/actions/action";
 
 const App = ()=> {
   const myState = useSelector((state)=> state.changeTheNumber);
   const dispatch = useDispatch();
   return (
     <>
-    
-
     <div class="container">
-
     <h1>Increment/Decrement counter</h1>
-    
     <div class="quantity">
       <a class="quantity__minus" title="Decrement" onClick={() => dispatch(decrementNum())}><span>-</span></a>
       <input name="quantity" type="text" class="quantity__input" value={myState} />
